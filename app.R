168b0582fe0b3e1b88452a06e44412e5db857197
Ricardo Pizarro <rik990@gmail.com>
2023-06-20 10:31:47 -0400

    probando calidad

--- app.R
+++ app.R
@@ -12,32 +12,42 @@ ui <- fluidPage(
   titlePanel("Probando paquete calidad del INE"),
   sidebarLayout(sidebarPanel(
     selectInput("var",label = "Variable de interés",choices = c("",names(enusc)),selected = ""),
-    #  selectInput("dominio",label = "Variable desagregacion",choices = c("",names(enusc)),selected = ""),
-    downloadButton("download_tabla","Descarga")),
-    mainPanel(tableOutput("tabla")
-    ))
+    selectInput("dominio",label = "Variable desagregacion",choices = c("",names(enusc)),selected = ""),
+    downloadButton("download_tabla","Descarga")
+  ),
+  mainPanel(tableOutput("tabla")
+  ))
 )
 ### SERVER ####
 server <- function(input, output, session) {
-  # R_dominio = reactive({
-  #   # print(input$dominio)
-  #
-  #   if(input$dominio == ""){
-  #     NULL
-  #   }else{
-  #     input$dominio
-  #   }
-  # })
+  R_dominio = reactive({
+    # print(input$dominio)
+    if(input$dominio == ""){
+      NULL
+    }else{
+      input$dominio
+    }
+  })
   ### generamos tabla
   tabulado <- reactive({
     calidad::assess(calidad::create_prop(var = input$var,
-                                         #denominator = #R_denom(),
-                                         #  domains = #R_dominio(),
+                                         domains = R_dominio(),
                                          design = com_dis))
   })
   output$tabla <- renderTable({
-    # req(input$var)
+    req(input$var)
+    # print(R_dominio)
+    # print(R_denom)
     tabulado()
   })
+
+  output$download_tabla <- downloadHandler(
+    filename = function() {
+      paste0("tabulado-", format(Sys.time(),"%Y-%m-%d-%H%M%S"), ".xlsx", sep="")
+    },
+    content = function(file) {
+      writexl::write_xlsx(tabulado(), file)
+    }
+  )
 }
 shinyApp(ui, server)
